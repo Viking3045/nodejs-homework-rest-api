76df0ecb3c18fe27fdc6d9b84d4e19fa263e6343
Vitaliy_Stepanenko <utihar30@gmail.com>
2023-08-07 22:15:36 +0300

    complete task

--- models/user.js
+++ models/user.js
@@ -22,7 +22,11 @@ const userSchema = new Schema(
           enum: ["starter", "pro", "business"],
           default: "starter"
         },
-        token: String
+        token: String,
+        avatarUrl:{
+          type: String,
+          required:true,
+        }
       }, {versionKey:false}
 )
 
